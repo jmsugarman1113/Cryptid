cc0a6691300f9ca1fc06c20fde64655e9b7268e7
Jonah Sugarman <jmsugarman1113@gmail.com>
2025-01-13 22:42:15 -0600

    outline of setup Card

---
+++ SetupCard.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from Hex import FixedLength, Hex
+from typing import Annotated
+from Clue import Clue
+from Tile import Structure
+
+
+@dataclass
+class SetupCard:
+    board_sections: Annotated[list[int], FixedLength(6)]
+    structures: list[tuple[Hex, Structure]]
+    clues: Annotated[dict[int, list[Clue]], FixedLength(3)]  # keys are guaranteed to be 3, 4, 5 for num players
