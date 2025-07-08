aee2577e654c40b056697ae3ea5dacc89f9df37e
Jonah Sugarman <jmsugarman1113@gmail.com>
2025-07-07 21:53:49 -0500

    confirmed pointy and flat drawing working as expected, and stitching together board sections

--- tests/drawing.py
+++ tests/drawing.py
@@ -1,12 +1,13 @@
 import math
-from typing import Final
+from typing import Final, Iterable, Callable
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
 
 from cryptid.board_sections import BOARD_SECTIONS
+from cryptid.board import Board
 from cryptid.hex import AxialCoordinateHex
-from cryptid.tile import Terrain
+from cryptid.tile import Terrain, Tile, AnimalTerritory, Structure
 
 SQRT3: Final[float] = math.sqrt(3)
 SQRT3_OVER2: Final[float] = math.sqrt(3) / 2
@@ -19,32 +20,95 @@ HEX_COLOR_MAPPING: Final[dict[Terrain, str]] = {
     Terrain.MOUNTAIN: "gray",
 }
 
+ANIMAL_COLOR_MAPPING: Final[dict[AnimalTerritory, str]] = {
+    AnimalTerritory.COUGAR: "red",
+    AnimalTerritory.BEAR: "black",
+}
+
 
 def hex_to_pointy_coords(axial: AxialCoordinateHex, radius: float = 1) -> tuple[float, float]:
     x = radius * (SQRT3 * axial.q + SQRT3_OVER2 * axial.r)
     y = -radius * 1.5 * axial.r
-
     return x, y
 
 
-if __name__ == "__main__":
-    board = BOARD_SECTIONS[0]
-    r = 1
+def hex_to_flat_coords(axial: AxialCoordinateHex, radius: float = 1) -> tuple[float, float]:
+    x = 1.5 * radius * axial.q
+    y = -radius * (SQRT3 * axial.r + SQRT3_OVER2 * axial.q)
+    return x, y
 
-    fig, ax = plt.subplots(1)
-    # ax.set_aspect('equal')
 
-    # Add some coloured hexagons
-    for tile in board.tiles.values():
-        color = HEX_COLOR_MAPPING[tile.terrain]
-        axial = tile.hex.to_axial_coordinate_hex()
-        x, y = hex_to_pointy_coords(axial, radius=r)
-        hexagon = RegularPolygon(
-            (x, y), numVertices=6, radius=r, orientation=0, facecolor=color, alpha=0.2, edgecolor="k"
+def plot_hex(
+    tile: Tile,
+    radius: float,
+    text_size: int,
+    ax: plt.axes,
+    orientation: float,
+    coords_func: Callable[[AxialCoordinateHex, float], tuple[float, float]],
+    annotate_position: bool = False,
+) -> None:
+    color = HEX_COLOR_MAPPING[tile.terrain]
+    axial = tile.hex.to_axial_coordinate_hex()
+    x, y = coords_func(axial, radius)
+    hexagon = RegularPolygon(
+        (x, y), numVertices=6, radius=radius, orientation=orientation, facecolor=color, alpha=0.2, edgecolor="k"
+    )
+    ax.add_patch(hexagon)
+
+    if tile.animal_territory:
+        animal_color = ANIMAL_COLOR_MAPPING[tile.animal_territory]
+        animal_hexagon = RegularPolygon(
+            (x, y),
+            numVertices=6,
+            radius=2/3 * radius,
+            orientation=orientation,
+            edgecolor=animal_color,
+            alpha=.8,
+            fill=False,
+            linestyle='--'
         )
-        ax.add_patch(hexagon)
-        # Also add a text label
-        ax.text(x, y + 0.2, (axial.q, axial.r), ha="center", va="center", size=8)
-        ax.text(x, y - 0.2, (tile.hex.q, tile.hex.r), ha="center", va="center", size=8)
+        ax.add_patch(animal_hexagon)
+    # Also add a text label
+    if annotate_position:
+        ax.text(x, y + 0.2, (axial.q, axial.r), ha="center", va="center", size=text_size)
+        ax.text(x, y - 0.2, (tile.hex.q, tile.hex.r), ha="center", va="center", size=text_size)
+
+
+def plot_pointy(tiles: Iterable[Tile], radius: float = 1, text_size: int = 5, annotate_position: bool = False) -> None:
+    fig, ax = plt.subplots(1)
+
+    for tile in tiles:
+        plot_hex(
+            tile=tile,
+            radius=radius,
+            text_size=text_size,
+            orientation=0,
+            coords_func=hex_to_pointy_coords,
+            ax=ax,
+            annotate_position=annotate_position,
+        )
 
     plt.show()
+
+
+def plot_flat(tiles: Iterable[Tile], radius: float = 1, text_size: int = 5, annotate_position: bool = False) -> None:
+    fig, ax = plt.subplots(1)
+
+    for tile in tiles:
+        plot_hex(
+            tile=tile,
+            radius=radius,
+            text_size=text_size,
+            orientation=math.pi/6,
+            coords_func=hex_to_flat_coords,
+            ax=ax,
+            annotate_position=annotate_position,
+        )
+    plt.show()
+
+
+if __name__ == "__main__":
+    # board = BOARD_SECTIONS[0]
+    board = Board.from_board_sections([1,2,3,4,5,6])
+    plot_pointy(board.tiles.values())
+    plot_flat(board.tiles.values())
